5e18d164bf616bad949b41100a5debfe33083225
Roger Chappel <roger@example.local>
2026-05-11 18:38:36 +1000

    Export public library API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { explainCommand } from "./classifier.js";
+export { scan } from "./scan.js";
+export { toJson, toMarkdown } from "./reporters.js";
+export type { CmdMapConfig, CommandFinding, ScanResult } from "./types.js";
